e69994185d3af6d8d4345e267a0cae6871d1d40b
cosmos-fly <coder211@163.com>
2021-09-10 09:38:21 +0800

    WF53: ScotPlot 模拟静态图

--- WinFormsApp01/FrmMobus.cs
+++ WinFormsApp01/FrmMobus.cs
@@ -15,9 +15,24 @@ namespace WinFormsApp01
             InitializeComponent();
         }
 
+
+        public double[] GetRandomNum(int length)
+        {
+            double[] getDate = new double[length];
+            Random random = new Random(); //创建一个Random实例
+            for (int i = 0; i < length; i++)
+            {
+                getDate[i] = random.Next(1, 100); //使用同一个Random实例生成随机数
+            }
+            return getDate;
+        }
         private void FrmMobus_Load(object sender, EventArgs e)
         {
 
+            double[] dataX = GetRandomNum(20).Distinct().OrderByDescending(x => x).ToArray();
+            double[] dataY = GetRandomNum(19).Distinct().OrderByDescending(x => x).ToArray();
+            frmPlot.Plot.Add.Scatter(dataX, dataY);
+            frmPlot.Refresh();
         }
 
         private void button1_Click(object sender, EventArgs e)
